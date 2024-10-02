9ed77c31c61cc96b953ae4bac4b7218b649cbf18
mikaelelias <mikael.mesg@gmail.com>
2024-10-01 21:48:33 -0300

    Finalizing styles and creating routes.

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,32 +1,62 @@
 import api from '../../services/api'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 import Button from "../../components/Button"
 import TopBackground from "../../components/TopBackground"
+import Trash from '../../assets/trash.svg'
 
+import { Container, 
+        ContainerUsers,
+        CardUsers, 
+        TrashIcon, 
+        Title,
+        AvatarUsers } from './styles'; // Make sure AvatarUsers is imported
 
 function ListUsers() {
+    const [users, setUsers] = useState([])
+    const navigate = useNavigate();
+
 
     useEffect(() => {
-
         async function getUsers() {
-            const usersFromApi = await api.get('/usuarios')
-            console.log(usersFromApi)
+            const { data } = await api.get('/usuarios')
+            console.log(data)
+            setUsers(data)
         }
         getUsers()
     }, [])
 
+async function deleteUsers(id) {
+    await api.delete(`/usuarios'/${id}`)
+
+    const updatedUsers = users.filter( user => user.id !== id )
+
+    setUsers(updatedUsers)
+}
+
     return (
-
-        <div>
-
+        <Container>
             <TopBackground />
-            <h1>Listagem de usuários</h1>
-            <Button>voltar</Button>
-            
-        </div>
+            <Title>Cadastrar Usuários</Title>
 
+            <ContainerUsers>
+                {users.map(user => (
+                    <CardUsers key={user.id}> {/* Add key here */}
+                        <AvatarUsers src={`https://avatar.iran.liara.run/public?username=${user.id}`}/>
+                        <div>
+                            <h3>Nome: {user.name}</h3>
+                            <p>Idade: {user.age}</p>
+                            <p>Email: {user.email}</p>
+                        </div>
+                        <TrashIcon src={Trash} alt='icon-lixo' onClick={() => deleteUsers(users.id)}/>
+                    </CardUsers>
+                ))}
+            </ContainerUsers>
+
+            <Button type='button' onClick={() => navigate('/')}>Voltar</Button>     
+        </Container>
     )
 }
 
-export default ListUsers
+export default ListUsers;
--- src/pages/ListUsers/styles.js
+++ src/pages/ListUsers/styles.js
@@ -1,32 +1,73 @@
 import styled from "styled-components";
 
-export const TopBackground = styled.div`
-    background-color: #f0f0f0; /* Cor de fundo */
-    height: 200px; /* Altura do fundo */
+export const Container = styled.div`
+  background-color: #181f26;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 200px;
+`;
+
+export const CardUsers = styled.div`
+    background-color: #252d48;
+    padding: 16px;
+    border-radius: 32px;
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
+    gap: 20px;
+    max-width: 500px;
+
+    h3 {
+        color: #fff;
+        font-size: 24px;
+        margin-bottom: 3px;
+        text-transform: capitalize;
+}
+
+    p {
+        color: #fff;
+        font-size: 14px;
+        font-weight: 200px;
+}
+`;
+
+
+export const ContainerUsers = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;  
+    margin: 40px 0;
+
+    @media (max-width: 886px) {
+       grid-template-columns: 1fr;
+    }
+`;
+
+export const Title = styled.div`
+    color: #ffffff;
     text-align: center;
-    font-size: 24px; /* Tamanho da fonte */
-    color: #333; /* Cor do texto */
+    font-size: 38px;
+    font-style: normal;
+    font-weight: 600;
+    padding: 30px;
+`;
+
+export const TrashIcon = styled.img`
+  cursor: pointer;
+  padding-left: 30px;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:active {
+    opacity: 0.5;
+  }
+`;
+
+export const AvatarUsers = styled.img`
+    height: 80px;
 `
-
-export const Button = styled.button`
-    border: none;
-    background: linear-gradient(180deg, #fe7e5d 0%, #ff6378 100%);
-    font-size: 16px;
-    color: #fff;
-    padding: 16px 32px;
-    width: fit-content;
-    cursor: pointer;
-    border-radius: 30px;
-
-    &:hover {
-        opacity: 0.8;
-    }
-
-    &:active {
-        opacity: 0.5;
-    }
-`
-
